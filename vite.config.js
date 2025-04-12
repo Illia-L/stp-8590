8929141a02546333a13ffb30c82bfc6e5a7a2b2f
Illia Liashko <2275040@gmail.com>
2025-04-12 11:46:54 +0300

    fix-images

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import { globalStylesOptions } from './global.styles';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/stp-8590/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
